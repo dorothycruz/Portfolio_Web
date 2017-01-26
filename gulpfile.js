4bd6f4291543564db0f4d71d61da3442601ea655
Roy Sanchez <Roy Sanchez>
2017-01-25 23:26:47 -0400

    Created the package.json file with these deps: *gulp. *node-sass. *gulp-sass. *browser-sync.
    For the following uses:
    *Gulp to sync both sass and browsersync.
    *Sass to compile the scss files to css.
    *Browser-sync to run a local server in development.
    
    deleted the server.js as it's no longer needed.

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+
+// Static Server + watching scss/html files
+gulp.task('default', ['sass'], function () {
+
+    browserSync.init({
+        server: ".",
+        port: 8080
+    });
+
+    gulp.watch("./scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function () {
+    return gulp.src("./scss/*.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            includePaths: require('bourbon').includePaths
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("./css"))
+        .pipe(browserSync.stream());
+});
